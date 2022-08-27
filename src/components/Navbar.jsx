7e9d68d5b5eec57bc1b8da2356796329970d0cc4
unknown <ashucoc98765@gmail.com>
2022-08-27 23:03:34 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return( 
+    <nav className="nav-container">
+      <img src="../../src/assets/earth-icon.svg" alt="Earth icon" />
+      <p>my travel journal.</p>
+    </nav>
+  )
+}
